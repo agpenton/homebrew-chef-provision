e7ffb8b3f3814a4cbce2d367d6213d18c1f28a63
Asdrubal Gonzalez <agpenton@Asdrubals-iMac.local>
2020-06-23 15:42:11 +0400

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook:: base
+# Recipe:: default
+#
+# The MIT License (MIT)
+#
+# Copyright:: 2020, Asdrubal Gonzalez Penton
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+%w(autoconf bash-completion doxygen gettext git iperf sqlite nmap ssh-copy-id pv\
+   wget wrk httpie zsh-completions mackup mas python awscli packer\
+   packer-completion neofetch ruby).each do |b|
+     homebrew_package b do
+       action :install
+     end
+
+%w(dropbox the-unarchiver iterm2 spotify transmission firefox docker virtualbox\
+  skype slack vagrant terraform macvim visual-studio-code jetbrains-toolbox).each do |c|
+  homebrew_cask c do
+    action :install
+  end
+
+homebrew_update 'all platforms' do
+  frequency 86400
+  action :periodic
+end
