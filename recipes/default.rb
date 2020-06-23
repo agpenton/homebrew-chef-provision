88054a4437412c50fd10b948b2b39fdfaa3227fd
Asdrubal Gonzalez <agpenton@Asdrubals-iMac.local>
2020-06-23 18:55:10 +0400

    fixing the UID and GUID

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,8 +23,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-user = `whoami`
-group = 'staff'
+user = `id -u $(whoami)`
+group = 'id -g $(whoami)'
 
 homebrew_tap 'homebrew/core' do
   action :tap
